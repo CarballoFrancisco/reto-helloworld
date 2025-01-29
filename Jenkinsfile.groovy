c9dfc7512007d5f413090556ff736f4910ca3186
Francisco Manuel Carballo Alamo <97671162+CarballoFrancisco@users.noreply.github.com>
2025-01-29 20:35:02 +0100

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -2,76 +2,89 @@ pipeline {
     agent any
 
     stages {
-        stage('Clonar repositorio y verificar archivos') {
+        stage('Get Code') {
             steps {
                 git 'https://github.com/CarballoFrancisco/reto-helloworld.git'
                 bat 'dir'
+                echo "WORKSPACE: ${env.WORKSPACE}"
             }
         }
 
-        stage('Verificar espacio de trabajo') {
+        stage('Unit') {
+            steps {
+                catchError(buildResult: 'UNSTABLE', stageResult: 'FAILURE') {
+                    bat 'set PYTHONPATH=%WORKSPACE%\\'
+                    bat 'python -m pytest --junitxml=result-unit.xml test\\unit'
+                    junit 'result*.xml'
+                    bat 'python -m coverage run --branch --source=app --omit=app\\_init.py,app\\api.py -m pytest test\\unit'
+                    bat 'python -m coverage xml'
+                }
+            }
+        }
+
+        stage('Rest') {
             steps {
                 script {
-                    echo "El espacio de trabajo es: ${env.WORKSPACE}"
+                    echo 'Iniciando WireMock en Docker'
+                    bat '''
+                        docker run -d --name wiremock -p 9090:8080 wiremock/wiremock:latest
+                    '''
                 }
             }
         }
 
-        stage('Build') {
+         stage('Static') {
             steps {
-                // Simulación de sobrecarga
+                catchError(buildResult: 'UNSTABLE', stageResult: 'FAILURE') {
+                    script {
+                        bat 'flake8 --exit-zero --format=pylint app >flake8.out'
+                        recordIssues tools: [flake8(name: 'Flake8', pattern: 'flake8.out')],
+                                     qualityGates: [
+                                         [threshold: 8, type: 'TOTAL', unstable: true],
+                                         [threshold: 10, type: 'TOTAL', unstable: false]
+                                     ]
+                    }
+                }
+            }
+        }
+
+        stage('Security') {
+            steps {
+                script {
+                    bat '''
+                        bandit --exit-zero -r . -f custom -o bandit.out --msg-template "{abspath}:{line}: [{test_id}]: {msg}"
+                    '''
+                }
+                recordIssues tools: [pyLint(name: 'Bandit', pattern: 'bandit.out')],
+                             qualityGates: [
+                                 [threshold: 2, type: 'TOTAL', unstable: true],
+                                 [threshold: 4, type: 'TOTAL', unstable: false]
+                             ]
+            }
+        }
+
+        stage('Coverage') {
+            steps {
+                script {
+
+                    cobertura coberturaReportFile: 'coverage.xml',
+                              lineCoverageTargets: '95,0,85',
+                              conditionalCoverageTargets: '90,0,80',
+                              onlyStable: false
+                }
+            }
+        }
+
+        stage('Performance') {
+            steps {
                 bat '''
-                    python -c "import math; [math.sqrt(i) for i in range(10000000)]"
+                    set FLASK_APP=C:/ProgramData/Jenkins/.jenkins/workspace/test1/app/api.py
+                    start /B flask run
+                    "C:\\Users\\carba\\Desktop\\apache-jmeter-5.6.3\\apache-jmeter-5.6.3\\bin\\jmeter" -n -t "test\\jmeter\\flask.jmx" -f -l "flask.jtl"
                 '''
-            }
-        }
-
-        stage('Pruebas en paralelo') {
-            parallel {
-                stage('Unit') {
-                    steps {
-                        catchError(buildResult: 'UNSTABLE', stageResult: 'FAILURE') {
-                            bat 'set PYTHONPATH=%WORKSPACE%\\'
-                            bat 'python -m pytest --junitxml=result-unit.xml test\\unit'
-                        }
-                    }
-                }
-                stage('Rest') {
-                    steps {
-                        script {
-                            // Iniciar Wiremock y Flask
-                            bat '''
-                                docker run -d --name wiremock -p 9090:8080 -v C:/ProgramData/Jenkins/.jenkins/workspace/casoPractico1/test/wiremock/mappings:/home/wiremock/mappings wiremock/wiremock:latest
-                                set FLASK_APP=C:/ProgramData/Jenkins/.jenkins/workspace/reto1/app/api.py
-                                start /B python -m flask run --host=0.0.0.0 --port=5000
-                            '''
-                            
-                            // Esperar a que los servicios estén listos (alternativa a timeout)
-                            echo "Esperando que los servicios estén listos..."
-                            bat '''
-                                ping -n 31 127.0.0.1 > nul
-                            '''
-                            
-                            // Ejecutar pruebas REST
-                            bat '''
-                                set PYTHONPATH=%WORKSPACE%
-                                pytest --junitxml=result-rest.xml test\\rest
-                            '''
-                        }
-                    }
-                }
-            }
-        }
-        
-        stage('Results') {
-            steps {
-                // Publicar los resultados de las pruebas
-                junit 'result-unit.xml'
-                junit 'result-rest.xml'
+                perfReport sourceDataFiles: 'flask.jtl'
             }
         }
     }
 }
-
-
 
