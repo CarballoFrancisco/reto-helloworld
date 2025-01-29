2c0e28167a0aeafba1681510a7763ca9ea82bdb8
Francisco Manuel Carballo Alamo <97671162+CarballoFrancisco@users.noreply.github.com>
2025-01-29 22:20:29 +0100

    Update calc.py

--- app/calc.py
+++ app/calc.py
@@ -1,14 +1,16 @@
 import app
 
+
 class InvalidPermissions(Exception):
     pass
+
 
 class Calculator:
     def add(self, x, y):
         self.check_types(x, y)
         return x + y
 
-    def subtract(self, x, y):  # Nombre corregido de 'substract' a 'subtract'
+    def substract(self, x, y):
         self.check_types(x, y)
         return x - y
 
@@ -19,7 +21,8 @@ class Calculator:
     def divide(self, x, y):
         self.check_types(x, y)
         if y == 0:
-            raise ZeroDivisionError("Division by zero is not possible")  # Excepción corregida
+            raise                                                TypeError("Division by zero is not possible")
+
         return x / y
 
     def power(self, x, y):
@@ -27,12 +30,12 @@ class Calculator:
         return x ** y
 
     def check_types(self, x, y):
-        if not isinstance(x, (int, float)) or not isinstance(y, (int, float)):
+        if not isinstance(x, (int, float)) or                                                not isinstance(y, (int, float)):
             raise TypeError("Parameters must be numbers")
+
 
 if __name__ == "__main__":  # pragma: no cover
     calc = Calculator()
     result = calc.add(2, 2)
     print(result)
-
 
