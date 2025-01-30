d2be260b03a9a382b856cda12323a109115b8c6b
Francisco Manuel Carballo Alamo <97671162+CarballoFrancisco@users.noreply.github.com>
2025-01-30 14:22:43 +0100

    Update calc.py

--- app/calc.py
+++ app/calc.py
@@ -19,7 +19,7 @@ class Calculator:
     def divide(self, x, y):
         self.check_types(x, y)
         if y == 0:
-            raise ZeroDivisionError("Division by zero is not possible")
+            ra
         return x / y
 
     def power(self, x, y):
